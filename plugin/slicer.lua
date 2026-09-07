71beb3d6ed78364877dce8d2a21c867e533135c5
pepedinho <2spii94@gmail.com>
2026-09-07 15:55:05 +0200

    try to fix init issue

--- plugin/slicer.lua
+++ plugin/slicer.lua
@@ -3,22 +3,21 @@ if vim.g.loaded_slicer == 1 then
 end
 vim.g.loaded_slicer = 1
 
-local slicer = require("slicer")
-
+-- On définit les commandes utilisateur sans importer immédiatement le module Lua
 vim.api.nvim_create_user_command("SlicerStart", function(opts)
 	local duration = tonumber(opts.args)
-	slicer.start(duration and (duration * 60) or nil)
+	require("slicer").start(duration and (duration * 60) or nil)
 end, { nargs = "?" })
 
 vim.api.nvim_create_user_command("SlicerExtend", function(opts)
 	local duration = tonumber(opts.args)
-	slicer.extend(duration and (duration * 60) or nil)
+	require("slicer").extend(duration and (duration * 60) or nil)
 end, { nargs = "?" })
 
 vim.api.nvim_create_user_command("SlicerPause", function()
-	slicer.toggle_pause()
+	require("slicer").toggle_pause()
 end, {})
 
 vim.api.nvim_create_user_command("SlicerStop", function()
-	slicer.stop()
+	require("slicer").stop()
 end, {})
